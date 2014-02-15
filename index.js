68e6f772018ec855cba1eff09a5e81490fb3a049
Luis Arriojas <luisarriojas@gmail.com>
2014-02-15 16:18:53 -0430

    problem with http module was fixed

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ var url = require("url");
 var fs = require("fs");
 var server;
 
-server.createServer(function (req, res) {
-    console.log("jojoy alguien se conecto!!     ");
 
-
-
-
+server = http.createServer(function (req, res) {
+    var path = url.parse(req.url).pathname;
+    switch (path) {
+        case "/":
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write("jojoy hola !!");
+            res.end();
+            break;
+    }
 });
 
-server.listen(8080);
+server.listen(1337);
