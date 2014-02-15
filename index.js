b5c568254fffe91436a4c27bbd93ac20672a2c46
Luis Arriojas <luisarriojas@gmail.com>
2014-02-15 16:03:33 -0430

    First problem with http module.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-/* =================================================
-   nodejs-static-noangularjs
-   Copyright (c) 2014 Luis Enrique Arriojas Catalini
-   http://opensource.org/licenses/MIT
-   ================================================= */
+/*
+ =================================================
+ nodejs-static-noangularjs
+ Copyright (c) 2014 Luis Enrique Arriojas Catalini
+ http://opensource.org/licenses/MIT
+ =================================================
+ */
 
-console.log("hello world!!");
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var server;
+
+server.createServer(function (req, res) {
+    console.log("jojoy alguien se conecto!!     ");
 
 
 
+
+});
+
+server.listen(8080);
