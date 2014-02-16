9788970864e634ba9aec990b8a9db7965d0b7934
Luis Arriojas <luisarriojas@gmail.com>
2014-02-15 23:02:47 -0430

    Code optimized. The project becomes in a webserver to serve static websites.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 nodejs-static-noangularjs
-====================
+=========================
 
 Static website without AngularJS using Node.js
+
+### How to use:
+1) Modify the Router section on index.js.
+2) Add files to the folder's structure of your choice.
+
+This boilerplate comes with:
+* A "static" folder to store .html files.
+* A "css" folder to store .css files.
+* An "img" folder to store .jpg files.
+* You can create a "js" folder to store .js files or whatever you want to do.
--- index.js
+++ index.js
@@ -10,74 +10,52 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 var server;
+var error404Url = "/static/404.html";
 
 server = http.createServer(function (req, res) {
     var path = url.parse(req.url).pathname;
     switch (path) {
+        /* Router section*/
         case "/":
-            fs.readFile(__dirname + "/static/home.html", function (error, data) {
-                if (error) return send404Html(res);
-
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(data, "utf8");
-                res.end();
-            });
-            break;
+            callAsset("/static/home.html", res);
         case "/home":
-            fs.readFile(__dirname + "/static/home.html", function (error, data) {
-                if (error) return send404Html(res);
-
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(data, "utf8");
-                res.end();
-            });
-            break;
+            callAsset("/static/home.html", res);
         case "/about":
-            fs.readFile(__dirname + "/static/about.html", function (error, data) {
-                if (error) return send404Html(res);
-
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(data, "utf8");
-                res.end();
-            });
-            break;
+            callAsset("/static/about.html", res);
         case "/contact":
-            fs.readFile(__dirname + "/static/contact.html", function (error, data) {
-                if (error) return send404Html(res);
-
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(data, "utf8");
-                res.end();
-            });
-            break;
+            callAsset("/static/contact.html", res);
+        /*End of Router section */
         default:
-            fs.readFile(__dirname + path, function (error, data) {
-                if (error) return send404Html(res);
-
-                res.writeHead(200);
-                res.write(data);
-                res.end();
-            });
-            break;
+            callAsset(path, res);
     }
 });
 
 server.listen(1337);
 
-send404Html = function (res) {
-    fs.readFile(__dirname + "/static/404.html", function (error, data) {
-        if (error) return send404Text(res);
-
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.write(data, "utf8");
-        res.end();
-    });
-};
-
 send404Text = function (res) {
-    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.writeHead(404);
     res.write("404 error - The requested page wasn't found. Sorry.");
     res.end();
 };
 
-console.log("Ready!!");
+send404Html = function (res) {
+    fs.readFile(__dirname + error404Url, function (error, data) {
+        if (error) return send404Text(res);
+
+        res.writeHead(404);
+        res.write(data);
+        res.end();
+    });
+};
+
+callAsset = function (asset, res) {
+    fs.readFile(__dirname + asset, function (error, data) {
+        if (error) return send404Html(res);
+
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    });
+};
+
+console.log("Deployed!!");
